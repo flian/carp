9f455d04173a1d3e1fe44f4e46b555c23c1857cc
Foy Lian <ljs2342003@gmail.com>
2018-05-22 11:26:14 +0800

    freemarker config

---
+++ carp-base/src/main/java/org/lotus/carp/base/config/CarpConfig.java
@@ -0,0 +1,27 @@
+package org.lotus.carp.base.config;
+
+import lombok.Data;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+/**
+ * Carp 框架配置
+ *
+ * @author: Foy Lian
+ * Date: 5/7/2018
+ * Time: 4:47 PM
+ */
+@Configuration
+@ConfigurationProperties(prefix = "carp.config")
+@Data
+@Component("carpConfig")
+@ConditionalOnMissingBean
+public class CarpConfig {
+    private String defaultTitle;
+    private String version;
+    private String versionStatus;
+    private String[] exposeBeanNames;
+    private String[] exposeStaticClasses;
+}
