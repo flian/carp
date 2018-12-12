6fa5af0b506454be94f65bdd40fa1a638cd7c40c
Foy Lian <ljs2342003@gmail.com>
2018-12-12 16:42:09 +0800

    random 工具

--- carp-base/src/main/java/org/lotus/carp/base/crypt/RandomUtils.java
+++ carp-base/src/main/java/org/lotus/carp/base/crypt/RandomUtils.java
@@ -45,8 +45,12 @@ public class RandomUtils {
         private final char[] buf;
 
         public RandomString(int length, Random random, String symbols) {
-            if (length < 1) throw new IllegalArgumentException();
-            if (symbols.length() < 2) throw new IllegalArgumentException();
+            if (length < 1) {
+                throw new IllegalArgumentException();
+            }
+            if (symbols.length() < 2) {
+                throw new IllegalArgumentException();
+            }
             this.random = Objects.requireNonNull(random);
             this.symbols = symbols.toCharArray();
             this.buf = new char[length];
